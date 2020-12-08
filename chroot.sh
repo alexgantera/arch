f127cd4b0fb8c96df2339efc5bbdaf49c8fda86b
Ganter <alexgantera@gmail.com>
2020-12-08 15:39:38 +0300

    My setup

--- chroot.sh
+++ chroot.sh
@@ -407,7 +407,7 @@ rm -r /usr/share/icons
 rm -r /usr/share/sddm
 
 cd /home/$username/system/
-rsync -r -t -v --progress -l -s  etc root /
+rsync -r -t -v --progress -l -s etc root /
 rsync -r -t -v --progress -l -s icons /usr/share
 rsync -r -t -v --progress -l -s sddm /usr/share
 rsync -r -t -v --progress -l -s FullRepresentation.qml /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/ui
