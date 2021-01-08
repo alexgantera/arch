34ba8194046207b60bc027d3d7f803cc4279fd05
Ganter <alexgantera@gmail.com>
2021-01-08 12:52:43 +0300

    Исправления

--- chroot.sh
+++ chroot.sh
@@ -355,11 +355,10 @@ wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/arc-kde/maste
 
 git clone https://aur.archlinux.org/pamac-tray-icon-plasma.git
 cd /home/$username
-git clone https://aur.archlinux.org/pamac-tray-icon-plasma
 chown -R $username:users /home/$username/pamac-tray-icon-plasma
 chown -R $username:users /home/$username/pamac-tray-icon-plasma/PKGBUILD
 cd /home/$username/pamac-tray-icon-plasma
-sudo -u $username  makepkg -si --noconfirm
+sudo -u $username makepkg -si --noconfirm
 rm -Rf /home/$username/pamac-tray-icon-plasma
 
 
