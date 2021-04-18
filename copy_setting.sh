138b4a10bbc4f30a84ea08c395b9ad4ffc0ec5c4
Ganter <alexgantera@gmail.com>
2021-04-18 23:03:22 +0300

    Копирование

--- copy_setting.sh
+++ copy_setting.sh
@@ -2,7 +2,9 @@
 
 sudo mkdir -p system/icons $dir
 sudo rsync -r -t -v --progress -l -s /etc /root system
+
 sudo rsync -r -t -v --progress -l -s /usr/share/icons/My_icons system/icons
 sudo rsync -r -t -v --progress -l -s /usr/share/icons/default system/icons
 sudo rsync -r -t -v --progress -l -s /usr/share/icons/Qogir-white-cursors system/icons
+
 sudo rsync -r -t -v --progress -l -s /usr/share/sddm system
