2c6b52d734b1e353dbc9183466536b6d0aef5132
Ganter <alexgantera@gmail.com>
2021-10-25 16:26:50 +0300

    Копирование nano

--- copy_setting.sh
+++ copy_setting.sh
@@ -8,9 +8,13 @@ sudo rsync -r -t -v -a --progress -l -s /usr/share/icons/default system/icons
 sudo rsync -r -t -v -a --progress -l -s /usr/share/icons/Qogir-white-cursors system/icons
 
 sudo rsync -r -t -v -a --progress -l -s /usr/share/nano system
-sudo rsync -r -t -v -a --progress -l -s /usr/share/fish system
-sudo rsync -r -t -v -a --progress -l -s /usr/share/pipewire system
-sudo rsync -r -t -v -a --progress -l -s /usr/share/sddm system
 sudo rsync -r -t -v -a --progress -l -s /usr/share/nano-syntax-highlighting system
+
+sudo rsync -r -t -v -a --progress -l -s /usr/share/pipewire system
 sudo rsync -r -t -v -a --progress -l -s /usr/share/alsa-card-profile system
+
+sudo rsync -r -t -v -a --progress -l -s /usr/share/sddm system
+
+sudo rsync -r -t -v -a --progress -l -s /usr/share/fish system
+
 sudo rsync -r -t -v -a --progress -l -s /var/spool/cron system
